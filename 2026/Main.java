cb0f3518697ff9ad1cc39bf3a892fd729157a0ac
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-06-14 12:08:04 -0400

    minor fix

--- 2026/Main.java
+++ 2026/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); //work on it here if its java
+        System.out.println("Hello, World!"); //work on it here if its javasd
     }
 }
